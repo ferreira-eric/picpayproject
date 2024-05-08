f544b9086b1a506105ef3791f62d3070552b3c38
Eric Ferreira <ferreira14eric@gmail.com>
2024-05-08 11:14:34 -0300

    create Transactions Entity

---
+++ src/main/java/com/picpayproject/repository/entity/Transactions.java
@@ -0,0 +1,36 @@
+package com.picpayproject.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "transactions")
+public class Transactions implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+}
